d22d724723886dc117452c9f9e74bd351602c4a0
Mitch Aplin <mitchaplin@gmail.com>
2023-02-17 22:28:25 -0600

    Styling cleanup, add card flip animation

--- src/pages/collections/study/[id].tsx
+++ src/pages/collections/study/[id].tsx
@@ -1,14 +1,15 @@
 import {
-  CheckCircleIcon,
   ClockIcon,
-  MinusCircleIcon,
+  FireIcon,
+  HandThumbDownIcon,
+  HandThumbUpIcon,
   RectangleGroupIcon,
-  Square2StackIcon,
 } from "@heroicons/react/24/solid";
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import ReactCardFlip from "react-card-flip";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { useElapsedTime } from "use-elapsed-time";
 import LoadingSpinner from "../../../components/LoadingIcon";
@@ -18,7 +19,6 @@ import {
   renderRemainingTimer,
 } from "../../../components/utils/Timers";
 import { api } from "../../../utils/api";
-
 const StudySession: NextPage = () => {
   const router = useRouter();
   const { query } = useRouter();
@@ -89,6 +89,24 @@ const StudySession: NextPage = () => {
     setCurrentIndex(0);
   }, []);
 
+  const headers = [
+    {
+      name: "Collection Name",
+      stat: collection?.name,
+      image: <RectangleGroupIcon className="h-16 w-16 text-brand-offWhite" />,
+    },
+    {
+      name: "Total elapsed time",
+      stat: totalTime,
+      image: <ClockIcon className="h-16 w-16 text-brand-offWhite" />,
+    },
+    {
+      name: "Apples Earned",
+      stat: numCorrect,
+      image: AppleIcon,
+    },
+  ];
+
   const stats = [
     {
       name: "Collection Name",
@@ -104,6 +122,11 @@ const StudySession: NextPage = () => {
       name: "Time Elapsed",
       stat: totalTime,
       image: <ClockIcon className="h-16 w-16 text-brand-offWhite" />,
+    },
+    {
+      name: "Flash Cards Completed",
+      stat: flashCards.data?.length,
+      image: <FireIcon className="h-16 w-16 text-brand-offWhite" />,
     },
   ];
 
@@ -122,7 +145,7 @@ const StudySession: NextPage = () => {
         {currentIndex !== flashCards.data?.length && (
           <div className="col-span-4 flex justify-center rounded-lg">
             <dl className="mt-5 grid w-screen min-w-fit grid-cols-1 gap-5 rounded-lg xl:grid-cols-3">
-              {stats.map((item) => (
+              {headers.map((item) => (
                 <div
                   className="grid grid-cols-2 items-center justify-items-center overflow-hidden rounded-lg bg-gray-800"
                   key={item.name}
@@ -150,8 +173,8 @@ const StudySession: NextPage = () => {
         )}
         <section className="bg-brand-darkGray col-span-4 py-1">
           {currentIndex !== flashCards.data?.length && (
-            <div className="grid grid-cols-4 rounded-lg bg-gray-800 p-4 shadow-md transition-all ease-out">
-              <div className="col-span-1 grid gap-4">
+            <div className="grid grid-cols-5 gap-6">
+              <div className="col-span-5 grid gap-4 rounded-lg bg-gray-800 shadow-md lg:col-span-2">
                 <div className="flex flex-col justify-center">
                   {!showAnswer ? (
                     <div className="flex scale-90 justify-center p-1">
@@ -193,123 +216,118 @@ const StudySession: NextPage = () => {
                       </CountdownCircleTimer>
                     </div>
                   )}
-                  {!showAnswer ? (
-                    <div className="flex flex-row justify-center gap-8 p-4">
-                      <button
-                        onClick={() => {
-                          setShowAnswer(true);
-                        }}
-                        className="m-auto inline-flex items-center rounded-lg bg-brand-actionBlue px-3 py-2 text-center text-sm font-medium text-white hover:bg-brand-subtleBlue focus:outline-none focus:ring-4 focus:ring-blue-300"
-                      >
-                        View Answer
-                        <svg
-                          aria-hidden="true"
-                          className="ml-2 -mr-1 h-4 w-4"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                            clip-rule="evenodd"
-                          ></path>
-                        </svg>
-                      </button>
-                    </div>
-                  ) : (
-                    <div className="flex flex-row justify-center gap-8 p-4">
-                      <button
-                        onClick={() => {
-                          setCurrentIndex(currentIndex + 1),
-                            setShowAnswer(false),
-                            setNumCorrect(numCorrect + 1),
-                            setNumRecorded(numRecorded + 1);
-                        }}
-                        className="items-center rounded-lg bg-brand-actionBlue px-3 py-2 text-center text-sm font-medium text-white hover:bg-brand-subtleBlue focus:outline-none focus:ring-4 focus:ring-blue-300"
-                      >
-                        <CheckCircleIcon
-                          className="h-5 w-5"
-                          aria-hidden="true"
-                        />
-                      </button>
-                      <button
-                        onClick={() => {
-                          setCurrentIndex(currentIndex + 1),
-                            setShowAnswer(false),
-                            setNumRecorded(numRecorded + 1);
-                        }}
-                        className="items-center rounded-lg bg-red-600 px-3 py-2 text-center text-sm font-medium text-white hover:bg-red-300 focus:outline-none focus:ring-4 focus:ring-blue-300"
-                      >
-                        <MinusCircleIcon
-                          className="h-5 w-5"
-                          aria-hidden="true"
-                        />
-                      </button>
-                    </div>
-                  )}
+                  <div className="flex flex-row justify-center gap-8 p-4">
+                    <button
+                      disabled={!showAnswer}
+                      onClick={() => {
+                        setCurrentIndex(currentIndex + 1),
+                          setShowAnswer(false),
+                          setNumCorrect(numCorrect + 1),
+                          setNumRecorded(numRecorded + 1);
+                      }}
+                      className="items-center rounded-lg bg-brand-actionBlue px-3 py-2 text-center text-sm font-medium text-white hover:bg-brand-subtleBlue focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:bg-slate-500"
+                    >
+                      <HandThumbUpIcon className="h-5 w-5" aria-hidden="true" />
+                    </button>
+                    <button
+                      disabled={!showAnswer}
+                      onClick={() => {
+                        setCurrentIndex(currentIndex + 1),
+                          setShowAnswer(false),
+                          setNumRecorded(numRecorded + 1);
+                      }}
+                      className="items-center rounded-lg bg-red-600 px-3 py-2 text-center text-sm font-medium text-white hover:bg-red-300 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:bg-slate-500"
+                    >
+                      <HandThumbDownIcon
+                        className="h-5 w-5"
+                        aria-hidden="true"
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
-              <div className="text-body col-span-4 col-start-3  grid items-start gap-8 py-8 pl-16 text-start text-xl font-bold tracking-tight text-brand-offWhite xl:col-start-2">
-                <h1 className="text-heading text-3xl">
-                  {showAnswer ? "Answer" : `${"Question"}: ${currentIndex + 1}`}
-                </h1>
-                <p className="text-body text-xl">
-                  {showAnswer
-                    ? flashCards.data?.at(currentIndex)?.answer
-                    : flashCards.data?.at(currentIndex)?.question}
-                </p>
+              <div className="col-span-5 grid text-center lg:col-span-3">
+                <ReactCardFlip
+                  isFlipped={showAnswer}
+                  flipDirection="horizontal"
+                  flipSpeedBackToFront={1.25}
+                  flipSpeedFrontToBack={1.0}
+                >
+                  <div
+                    onClick={() => setShowAnswer(true)}
+                    className={`grid gap-4 rounded-lg bg-gray-800 p-6 font-heading text-brand-offWhite shadow-md ${
+                      !showAnswer ? "cursor-pointer" : "cursor-default"
+                    }`}
+                  >
+                    <h1 className="text-heading text-3xl">
+                      Question: {currentIndex + 1}
+                    </h1>
+                    <p className="text-body text-xl">
+                      {flashCards.data?.at(currentIndex)?.question}
+                    </p>
+                  </div>
+                  <div className="grid gap-4 rounded-lg bg-gray-800 p-6 font-heading text-brand-offWhite shadow-md ">
+                    <h1 className="text-heading text-3xl">Answer:</h1>
+                    <p className="text-body text-xl">
+                      {flashCards.data?.at(currentIndex)?.answer}
+                    </p>
+                  </div>
+                </ReactCardFlip>
               </div>
             </div>
           )}
           {numRecorded === flashCards.data?.length && (
-            <div className="grid grid-cols-4 rounded-lg bg-gray-800 p-4 shadow-md transition-all ease-out">
-              <div className="col-span-4 m-auto grid gap-4 pb-4 text-brand-offWhite">
-                <div className="grid grid-cols-4 gap-8">
-                  <div className="justify-left col-span-4 flex flex-row items-center">
-                    <RectangleGroupIcon className="h-16 w-16 text-brand-offWhite" />
-                    <p className="pl-4">
-                      Collection Name:&nbsp;{collection?.name}
-                    </p>
-                  </div>
-                  <div className="justify-left col-span-3 flex flex-row items-center">
-                    <Square2StackIcon className="h-16 w-16 text-brand-offWhite" />
-                    <p className="pl-4">
-                      Flash Cards Completed:&nbsp;{flashCards.data?.length}
-                    </p>
-                  </div>
-                  <div className="justify-left col-span-3 flex flex-row items-center">
-                    <ClockIcon className="h-16 w-16 text-brand-offWhite" />
-                    <p className="pl-4">Total elapsed time:&nbsp;{totalTime}</p>
-                  </div>
-                  <div className="justify-left col-span-3 flex flex-row items-center">
-                    <div>{AppleIcon}</div>
-                    <p className="pl-4">Apples Earned:&nbsp;{numCorrect}</p>
-                  </div>
-                </div>
-                <div className="flex flex-col lg:flex-row">
-                  <button
-                    onClick={(e) => void handleCreateStudySession(e)}
-                    className="bg-brand-dark m-auto mt-4 flex w-72 justify-center rounded-lg bg-brand-actionBlue px-5 py-2.5 text-sm font-medium text-brand-offWhite hover:bg-brand-subtleBlue focus:outline-brand-lightBlue"
-                  >
-                    Submit Study Session
-                  </button>
-                  <button
-                    onClick={(e) => {
-                      e.preventDefault();
-                      setTotalTime(0);
-                      setCurrentIndex(0);
-                      setShowAnswer(false);
-                      setNumCorrect(0);
-                      setNumRecorded(0);
-                    }}
-                    className="bg-brand-dark m-auto mt-4 flex w-72 justify-center rounded-lg bg-brand-actionBlue px-5 py-2.5 text-sm font-medium text-brand-offWhite hover:bg-brand-subtleBlue focus:outline-brand-lightBlue"
-                  >
-                    Start Over without Submitting
-                  </button>
-                </div>
+            <>
+              <div className="col-span-4 flex justify-center rounded-lg">
+                <dl className="mt-5 grid w-screen min-w-fit grid-cols-1 gap-5 rounded-lg xl:grid-cols-2 ">
+                  {stats.map((item) => (
+                    <div
+                      className="grid grid-cols-2 items-center justify-items-center overflow-hidden rounded-lg bg-gray-800"
+                      key={item.name}
+                    >
+                      <div className="ml-[-2rem] flex justify-start">
+                        {item.image}
+                      </div>
+                      <div className="flex w-full overflow-hidden">
+                        <div
+                          key={item.name}
+                          className="py-5 pr-4 text-right sm:pr-6"
+                        >
+                          <div className="text-left font-heading text-sm font-medium text-brand-cream">
+                            {item.name}
+                          </div>
+                          <div className="mt-1 flex w-40 items-start truncate pr-12 text-left font-body text-xl font-semibold tracking-tight text-brand-cream">
+                            {item.stat}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </dl>
               </div>
-            </div>
+
+              <div className="my-6 flex flex-col rounded-md bg-gray-800 p-4 lg:flex-row lg:justify-center lg:p-0">
+                <button
+                  onClick={(e) => void handleCreateStudySession(e)}
+                  className="bg-brand-dark m-auto my-2 flex w-72 justify-center rounded-lg bg-brand-actionBlue px-5 py-2.5 text-sm font-medium text-brand-offWhite hover:bg-brand-subtleBlue focus:outline-brand-lightBlue lg:m-4"
+                >
+                  Submit Study Session
+                </button>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTotalTime(0);
+                    setCurrentIndex(0);
+                    setShowAnswer(false);
+                    setNumCorrect(0);
+                    setNumRecorded(0);
+                  }}
+                  className="bg-brand-dark m-auto my-2 flex w-72 justify-center rounded-lg bg-brand-actionBlue px-5 py-2.5 text-sm font-medium text-brand-offWhite hover:bg-brand-subtleBlue focus:outline-brand-lightBlue lg:m-4"
+                >
+                  Start Over without Submitting
+                </button>
+              </div>
+            </>
           )}
         </section>
       </div>
